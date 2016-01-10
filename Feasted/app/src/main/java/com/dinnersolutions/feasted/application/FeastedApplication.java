f66531a9653a733507344ab9d6f94e0594d19004
dejanristic <dejanr23@gmail.com>
2016-01-10 16:38:43 -0500

    adding feasted application to override default

---
+++ Feasted/app/src/main/java/com/dinnersolutions/feasted/application/FeastedApplication.java
@@ -0,0 +1,14 @@
+package com.dinnersolutions.feasted.application;
+
+import android.app.Application;
+
+/**
+ * Created by Dejan Ristic on 1/10/16.
+ */
+public class FeastedApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
