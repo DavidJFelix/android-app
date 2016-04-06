19693d62d9f14ebe0d481a0123d6b1b064e21fe6
David J Felix <felix.davidj@gmail.com>
2016-04-06 18:56:50 -0400

    Move some of the service files to new names

---
+++ InstaMeal/app/src/main/java/com/dinnersolutions/instameal/api/InstamealsService.java
@@ -0,0 +1,120 @@
+package com.dinnersolutions.instameal.api;
+
+public interface InstamealsService {
+
+    /*
+    @POST("/addresses/")
+    @GET("/addresses/{id}")
+    @PATCH("/addresses/{id}")
+    @PUT("/addresses/{id}")
+    @DELETE("/addresses/{id}")
+     */
+
+
+    /*
+    @POST("allergens/")
+    @GET("/allergens/{id}")
+    @PATCH("/allergens/{id}")
+    @PUT("/allergens/{id}")
+    @DELETE("/allergens/{id}")
+     */
+    
+    
+    /*
+    @POST("/dietary_filters/")
+    @GET("/dietary_filters/{id}")
+    @PATCH("/dietary_filters/{id}")
+    @PUT("/dietary_filters/{id}")
+    @DELETE("/dietary_filters/{id}")
+     */
+    
+    
+    /*
+    @POST("/favorite_sellers/")
+    @GET("/favorite_sellers/{id}")
+    @PATCH("/favorite_sellers/{id}")
+    @PUT("/favorite_sellers/{id}")
+    @DELETE("/favorite_sellers/{id}")
+     */
+    
+    /*
+    @POST("/images/")
+    @GET("/images/{id}")
+    @PATCH("/images/{id}")
+    @PUT("/images/{id}")
+    @DELETE("/images/{id}")
+     */
+    
+    /*
+    @POST("/ingredients/")
+    @GET("/ingredients/{id}")
+    @PATCH("/ingredients/{id}")
+    @PUT("/ingredients/{id}")
+    @DELETE("/ingredients/{id}")
+     */
+
+    /*
+    @POST("/meals/")
+    @GET("/meals/{id}")
+    @PATCH("/meals/{id}")
+    @PUT("/meals/{id}")
+    @DELETE("/meals/{id}")
+     */
+    
+    /*
+    @POST("/orders/")
+    @GET("/orders/{id}")
+    @PATCH("/orders/{id}")
+    @PUT("/orders/{id}")
+    @DELETE("/orders/{id}")
+     */
+    
+    /*
+    @POST("/order_reviews/")
+    @GET("/order_reviews/{id}")
+    @PATCH("/order_reviews/{id}")
+    @PUT("/order_reviews/{id}")
+    @DELETE("/order_reviews/{id}")
+     */
+    
+    /*
+    @POST("/prices/")
+    @GET("/prices/{id}")
+    @PATCH("/prices/{id}")
+    @PUT("/prices/{id}")
+    @DELETE("/prices/{id}")
+     */
+    
+    /*
+    @POST("/reviews/")
+    @GET("/reviews/{id}")
+    @PATCH("/reviews/{id}")
+    @PUT("/reviews/{id}")
+    @DELETE("/reviews/{id}")
+     */
+    
+    /*
+    @POST("/seller_reviews/")
+    @GET("/seller_reviews/{id}")
+    @PATCH("/seller_reviews/{id}")
+    @PUT("/seller_reviews/{id}")
+    @DELETE("/seller_reviews/{id}")
+     */
+    
+    /*
+    - @POST("/users/")
+    @GET("/users/{id}")
+    @PATCH("/users/{id}")
+    @PUT("/users/{id}")
+    - @DELETE("/users/{id}")
+     */
+
+    /*
+    @GET("/auth/authorize")
+    @POST("/auth/convert-token")
+    Call<TokenResponse>
+    @POST("/auth/invalidate-sessions")
+    @POST("/auth/revoke-token")
+    @POST("/auth/token")
+     */
+}
